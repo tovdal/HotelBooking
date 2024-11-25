a0b529ca755d217d3f4c92921a8789fe31b658ea
Tova Dalin <tova.dalin@hotmail.com>
2024-11-25 17:28:08 +0100

    Add project files.

---
+++ HotelBooking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBooking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
