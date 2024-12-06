d9eab25c69c874aa28d5206345f91eb32e77f8b9
Tova Dalin <tova.dalin@hotmail.com>
2024-12-06 15:21:48 +0100

    Class Hotel Customer

---
+++ HotelBooking/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace HotelBooking.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; } // Maybe what this??
+        public string Adress { get; set; }
+
+        public CustomerStatus Status { get; set; } = CustomerStatus.Active;
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+    public enum CustomerStatus
+    {
+        Active,
+        Inactive,
+        Deleted
+    }
+}
