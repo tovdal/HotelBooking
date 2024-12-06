746c52002ac7acb3a30276b524b9a0447426a051
Tova Dalin <tova.dalin@hotmail.com>
2024-12-06 16:16:25 +0100

    CRUD funktions

---
+++ HotelBooking/CRUD/CRUDGuest.cs
@@ -0,0 +1,48 @@
+﻿using HotelBooking.Models;
+
+namespace HotelBooking.CRUD
+{
+    public class CRUDGuest
+    {
+        // Create
+        public void CreateNewGuest(Guest newGuest)
+        {
+            Guest.Add(newGuest);
+            // Richard said that this was important or it wont save to database
+            dbContext.SaveChanges(); // the dbContext not created yet
+        }
+
+        //Read
+        public List<Guest> GetActiveGuestInDatabase()
+        {
+            return
+        }
+        public List<Guest> GetDeteletGuestInDatabase()
+        {
+            return
+        }
+        public List<Guest> GetAllGuestInDatabase()
+        {
+            return
+        }
+
+        // Update
+        public void UpdateGuestStatus()
+        {
+
+        }
+        public void TakeBackSoftDeletedGuest()
+        {
+
+        }
+
+        // Delete
+        public void SoftDeleteGuest() 
+        {
+
+        }
+
+
+
+    }
+}
---
+++ HotelBooking/CRUD/CRUDRoom.cs
@@ -0,0 +1,51 @@
+﻿using HotelBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.CRUD
+{
+    public class CRUDRoom
+    {
+        public void CreateNewRoom(Room newRoom)
+        {
+            Guest.Add(newRoom);
+            // Richard said that this was important or it wont save to database
+            dbContext.SaveChanges(); // the dbContext not created yet
+        }
+
+        //Read
+        public List<Room> GetAvailableRoomsInDatabase()
+        {
+            return
+        }
+        public List<Room> GetTakenRoomsInDatabase()
+        {
+            return
+        }
+
+        public List<Room> GetDeteletRoomsInDatabase()
+        {
+            return
+        }
+
+
+        // Update
+        public void UpdateRoomStatus()
+        {
+
+        }
+        public void TakeBackSoftDeletedRoom()
+        {
+
+        }
+
+        // Delete
+        public void SoftDeleteRoom()
+        {
+
+        }
+    }
+}
