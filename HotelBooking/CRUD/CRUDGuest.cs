1202e8d7514ae2ad7a70615f10c0d246fd8f630c
Tova Dalin <tova.dalin@hotmail.com>
2024-12-06 18:13:27 +0100

    CRUD base for guest and room

--- HotelBooking/CRUD/CRUDGuest.cs
+++ HotelBooking/CRUD/CRUDGuest.cs
@@ -1,48 +1,48 @@
-﻿using HotelBooking.Models;
+﻿//using HotelBooking.Models;
 
-namespace HotelBooking.CRUD
-{
-    public class CRUDGuest
-    {
-        // Create
-        public void CreateNewGuest(Guest newGuest)
-        {
-            Guest.Add(newGuest);
-            // Richard said that this was important or it wont save to database
-            dbContext.SaveChanges(); // the dbContext not created yet
-        }
+//namespace HotelBooking.CRUD
+//{
+//    public class CRUDGuest
+//    {
+//        // Create
+//        public void CreateNewGuest(Guest newGuest)
+//        {
+//            Guest.Add(newGuest);
+//            // Richard said that this was important or it wont save to database
+//            dbContext.SaveChanges(); // the dbContext not created yet
+//        }
 
-        //Read
-        public List<Guest> GetActiveGuestInDatabase()
-        {
-            return
-        }
-        public List<Guest> GetDeteletGuestInDatabase()
-        {
-            return
-        }
-        public List<Guest> GetAllGuestInDatabase()
-        {
-            return
-        }
+//        //Read
+//        public List<Guest> GetActiveGuestInDatabase()
+//        {
+//            return
+//        }
+//        public List<Guest> GetDeteletGuestInDatabase()
+//        {
+//            return
+//        }
+//        public List<Guest> GetAllGuestInDatabase()
+//        {
+//            return
+//        }
 
-        // Update
-        public void UpdateGuestStatus()
-        {
+//        // Update
+//        public void UpdateGuestStatus()
+//        {
 
-        }
-        public void TakeBackSoftDeletedGuest()
-        {
+//        }
+//        public void TakeBackSoftDeletedGuest()
+//        {
 
-        }
+//        }
 
-        // Delete
-        public void SoftDeleteGuest() 
-        {
+//        // Delete
+//        public void SoftDeleteGuest() 
+//        {
 
-        }
+//        }
 
 
 
-    }
-}
+//    }
+//}
