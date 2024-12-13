1ac65ec798edf7519ca1dba52aed5f9a9b14d457
Tova Dalin <tova.dalin@hotmail.com>
2024-12-13 17:22:07 +0100

    Migrations made to SMSS SQL. Had to update the db because the models where updated.

--- HotelBooking/Migrations/20241213093943_RefactoredModels.Designer.cs
+++ HotelBooking/Migrations/20241213093943_RefactoredModels.Designer.cs
@@ -4,6 +4,7 @@ using HotelBooking.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelBooking.Migrations
 {
     [DbContext(typeof(HotelBookingDbContext))]
-    partial class HotelBookingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241213093943_RefactoredModels")]
+    partial class RefactoredModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -72,9 +75,6 @@ namespace HotelBooking.Migrations
                     b.Property<string>("FirstName")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<bool>("IsGuestDeleted")
-                        .HasColumnType("bit");
 
                     b.Property<bool>("IsGuestStatusActive")
                         .HasColumnType("bit");
---
+++ HotelBooking/Migrations/20241213152749_AddReworkedKeysInModels.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBooking.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReworkedKeysInModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGuestDeleted",
+                table: "Guests",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsGuestDeleted",
+                table: "Guests");
+        }
+    }
+}
