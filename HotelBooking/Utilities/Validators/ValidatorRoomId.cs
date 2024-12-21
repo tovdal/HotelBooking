903976ec5c7c066fc60ada060c80937f1f707832
Tova Dalin <tova.dalin@hotmail.com>
2024-12-21 19:45:23 +0100

    Added Validator for room ID. In the future i can make a generic ID validator for all IDs if i have time.

---
+++ HotelBooking/Utilities/Validators/ValidatorRoomId.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelBooking.Utilities.Validators
+{
+    public class ValidatorRoomId
+    {
+        public static bool TryGetRoomId(out int roomId)
+        {
+            Console.WriteLine("Enter the ID of the room: ");
+            var stringID = Console.ReadLine();
+
+            if (!int.TryParse(stringID, out roomId))
+            {
+                Console.WriteLine("Please enter a valid number ID.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
