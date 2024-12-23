a46eddd33c0ec3efac62af787d90f5778985588e
Tova Dalin <tova.dalin@hotmail.com>
2024-12-23 12:19:36 +0100

    Add Address property to Customer Model

--- HotelBooking/Models/Customer.cs
+++ HotelBooking/Models/Customer.cs
@@ -7,10 +7,9 @@
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
-        public string? Adress { get; set; }
+        public Address? Address { get; set; }
         public bool IsCustomerDeleted { get; set; } = false;
 
-        // En kund behöver inte ha en bokning för att vara gäst.
-        public List<Booking>? Bookings { get; set; } = new List<Booking>();
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
