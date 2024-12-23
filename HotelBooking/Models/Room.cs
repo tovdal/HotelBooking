c36ef652a62f11db8f5cd1eb1c27945dc80101bd
Tova Dalin <tova.dalin@hotmail.com>
2024-12-23 12:23:29 +0100

    Fix spelling errors Room

--- HotelBooking/Models/Room.cs
+++ HotelBooking/Models/Room.cs
@@ -5,16 +5,19 @@
         public int Id { get; set; }
         public int RoomNumber { get; set; }
         public byte RoomSize { get; set; }
-        public TypeOfRoom TypeOfRooms { get; set; }
+        public TypeOfRoom TypeOfRoom { get; set; }
         public decimal PricePerNight { get; set; }
         public bool IsAvailable { get; set; }
         public bool IsRoomDeleted { get; set; }
         public int? ExtraBedsCount { get; set; }
-        public bool IsExtraBedAvailable { get; set; } 
+        public bool IsExtraBedAvailable { get; set; }
+
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
     }
+
     public enum TypeOfRoom
     {
-        Singel,
+        Single,
         Double
     }
 }
