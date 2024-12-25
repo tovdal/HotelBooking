fbde49eb78b612ed86097004dde1f35b6524bb33
Tova Dalin <tova.dalin@hotmail.com>
2024-12-25 11:34:13 +0100

    Add bookingCalender for Check in and Check out date.

---
+++ HotelBooking/Utilities/Helpers/BookingInputCalenderHelper.cs
@@ -0,0 +1,58 @@
+﻿using Spectre.Console;
+
+namespace HotelBooking.Utilities.Helpers
+{
+    public class BookingInputCalenderHelper
+    {
+        public static string GetBookingDateCalendar(DateTime selectedDate)
+        {
+            var calendarContent = new StringWriter();
+
+            calendarContent.WriteLine($"[red]{selectedDate:MMMM}[/]".ToUpper());
+            calendarContent.WriteLine("Mon  Tue  Wed  Thu  Fri  Sat  Sun");
+            calendarContent.WriteLine("─────────────────────────────────");
+
+            DateTime firstDayOfMonth = new DateTime(selectedDate.Year, selectedDate.Month, 1);
+            int daysInMonth = DateTime.DaysInMonth(selectedDate.Year, selectedDate.Month);
+            int startDay = (int)firstDayOfMonth.DayOfWeek;
+            startDay = (startDay == 0) ? 6 : startDay - 1;
+
+            for (int i = 0; i < startDay; i++)
+            {
+                calendarContent.Write("     ");
+            }
+
+            for (int day = 1; day <= daysInMonth; day++)
+            {
+                if (day == selectedDate.Day)
+                {
+                    calendarContent.Write($"[green]{day,2}[/]   ");
+                }
+                else
+                {
+                    calendarContent.Write($"{day,2}   ");
+                }
+
+                if ((startDay + day) % 7 == 0)
+                {
+                    calendarContent.WriteLine();
+                }
+            }
+
+            return calendarContent.ToString();
+        }
+        public static void DisplayBookingDateCalendar(DateTime selectedDate)
+        {
+            string calendarContent = GetBookingDateCalendar(selectedDate);
+
+            var panel = new Panel(calendarContent)
+            {
+                Border = BoxBorder.Double,
+                Header = new PanelHeader($"[red]{selectedDate:yyyy}[/]", Justify.Center)
+            };
+
+            AnsiConsole.Write(panel);
+            AnsiConsole.MarkupLine("\nUse arrow keys [blue]\u25C4 \u25B2 \u25BA \u25BC[/] to \nnavigate and [green]Enter[/] to pick date.");
+        }
+    }
+}
