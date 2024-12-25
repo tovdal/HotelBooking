c2b8baaa95f4dc93d4d41be0de71245416c4b850
Tova Dalin <tova.dalin@hotmail.com>
2024-12-25 12:38:29 +0100

    Add bookingCreate.  BookingExists and IsRoomTaken

--- HotelBooking/Service/BookingService/BookingCreate.cs
+++ HotelBooking/Service/BookingService/BookingCreate.cs
@@ -1,5 +1,6 @@
 ﻿using HotelBooking.Data;
 using HotelBooking.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelBooking.Service.BookingService
 {
@@ -15,6 +16,22 @@ namespace HotelBooking.Service.BookingService
         {
             _dbContext.Add(newBooking);
             _dbContext.SaveChanges();
+        }
+
+        public bool BookingExists(int bookingId)
+        {
+            return _dbContext.Booking
+                .Any(booking => booking.Id == bookingId);
+        }
+
+        public bool IsRoomTaken(int roomNumber, DateTime checkInDate, DateTime checkOutDate)
+        {
+            return _dbContext.Set<Booking>()
+                .Include(booking => booking.Rooms)
+                .Any(booking =>
+                    booking.Rooms.Any(room => room.RoomNumber == roomNumber) &&
+                    booking.CheckInDate < checkOutDate &&
+                    booking.CheckOutDate > checkInDate);
         }
     }
 }
