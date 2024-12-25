fb7f35b397ed6d0a2690e0565d130b45ef8f42f4
Tova Dalin <tova.dalin@hotmail.com>
2024-12-25 14:16:34 +0100

    Add BookingCreateController

--- HotelBooking/Controllers/ControllerBooking/BookingCreateController.cs
+++ HotelBooking/Controllers/ControllerBooking/BookingCreateController.cs
@@ -1,5 +1,9 @@
 ﻿using HotelBooking.Controllers.ControllerBooking.Interface;
+using HotelBooking.Controllers.ControllerRooms.Interface;
 using HotelBooking.Service.BookingService;
+using HotelBooking.Service.RoomService;
+using HotelBooking.Utilities.Helpers;
+using HotelBooking.Utilities.Validators;
 using Spectre.Console;
 
 namespace HotelBooking.Controllers.ControllerBooking
@@ -7,24 +11,69 @@ namespace HotelBooking.Controllers.ControllerBooking
     public class BookingCreateController : IBookingCreateController
     {
         private readonly BookingCreate _bookingCreate;
-        public BookingCreateController(BookingCreate bookingCreate)
+        private readonly RoomRead _roomRead;
+        private readonly IRoomReadController _roomReadController;
+
+        public BookingCreateController(BookingCreate bookingCreate, IRoomReadController roomReadController)
         {
             _bookingCreate = bookingCreate;
+            _roomReadController = roomReadController;
         }
+
         public void CreateBooking()
         {
             bool IsRunning = true;
             while (IsRunning)
             {
-                AnsiConsole.MarkupLine("[bold green]1.Registrat a new booking[/]");
+                AnsiConsole.MarkupLine("[bold green]1. Register a new booking[/]");
 
                 bool confirmCustomer = AnsiConsole.Confirm("\nIs it a new customer that wishes to make a booking?");
                 if (confirmCustomer)
                 {
-                    AnsiConsole.MarkupLine("[bold green]Customer needs to be registerd first![/]");
+                    AnsiConsole.MarkupLine("[bold green]Customer needs to be registered first![/]");
                     Console.ReadKey();
                     break;
                 }
+                AnsiConsole.MarkupLine("[bold green]Type in customer ID[/]");
+                ValidatorCustomerId.TryGetCustomerId(out var customerId);
+
+                AnsiConsole.MarkupLine("[bold green]Pick check-in date[/]");
+                var selectedCheckInDate = BookingInputCalenderHelper.HandleUserInput(DateTime.Now);
+                if (selectedCheckInDate == DateTime.MinValue)
+                {
+                    AnsiConsole.MarkupLine("[bold red]Check-in date selection canceled.[/]");
+                    break;
+                }
+
+                AnsiConsole.MarkupLine("[bold green]Pick check-out date[/]");
+                var selectedCheckOutDate = BookingInputCalenderHelper.HandleUserInput(selectedCheckInDate, selectedCheckInDate);
+                if (selectedCheckOutDate == DateTime.MinValue)
+                {
+                    AnsiConsole.MarkupLine("[bold red]Check-out date selection canceled.[/]");
+                    break;
+                }
+
+                AnsiConsole.MarkupLine("[bold green]Available rooms:[/]");
+                _roomReadController.ShowAllRooms();
+
+                bool IsAddingRooms = true;
+                while (IsAddingRooms)
+                {
+                    string roomRoomNumber = AnsiConsole.Prompt(
+                        new TextPrompt<string>("Enter room number you want to book: ")
+                            .ValidationErrorMessage("[red]Invalid or room already booked![/]")
+                            .Validate(input =>
+                            {
+                                if (!int.TryParse(input, out int roomNumber)) return false;
+                                return !_bookingCreate.IsRoomBooked(roomNumber);
+                            })
+                    );
+
+                    IsAddingRooms = AnsiConsole.Confirm("\n[bold yellow]Want to add another room?[/]");
+                }
+
+                var totalBookingPrice = _bookingCreate.TotalPriceOfBooking();
+                Console.WriteLine($"The total price of all bookings is: {totalBookingPrice:C}");
 
                 bool confirm = AnsiConsole.Confirm("\n[bold yellow]Are all details correct?[/]");
                 if (confirm)
