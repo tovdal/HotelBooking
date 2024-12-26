261b82ed814fb605479ca5ded836dcc16964f66e
Tova Dalin <tova.dalin@hotmail.com>
2024-12-26 21:51:34 +0100

    Add CustomerRead CustomerExists

--- HotelBooking/Service/CustomerService/CustomerRead.cs
+++ HotelBooking/Service/CustomerService/CustomerRead.cs
@@ -12,25 +12,32 @@ namespace HotelBooking.Service.CustomerService
         public CustomerRead(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
-            //Console.WriteLine("DbContext is configured: " + (_dbContext != null));
         }
+
         public IQueryable<Customer> GetAllActiveCustomers()
         {
             return _dbContext.Customers
                 .Where(c => !c.IsCustomerDeleted);
-                               
         }
+
+        public bool CustomerExists(int customerId)
+        {
+            return _dbContext.Customers
+                .Any(c => c.Id == customerId && !c.IsCustomerDeleted);
+        }
+
         public IQueryable<Customer> GetAllDeletedCustomersInDatabase()
         {
             return _dbContext.Customers
-                .Where(g => g.IsCustomerDeleted)
-                ;
+                .Where(g => g.IsCustomerDeleted);
         }
+
         public IQueryable<Customer> GetCustomerDetailes(int id)
         {
             return _dbContext.Customers
                 .Where(g => g.Id == id);
         }
+
         public bool GetCustomersIsDeleted()
         {
             return _dbContext.Customers
