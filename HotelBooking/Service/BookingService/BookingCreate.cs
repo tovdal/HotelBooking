358ecb8b1d7dd1246237951522ec433f81166cfe
Tova Dalin <tova.dalin@hotmail.com>
2024-12-26 21:50:44 +0100

    Add booking service GetRoomsToBook, IsRoomBooked, AddRoomToBooking

--- HotelBooking/Service/BookingService/BookingCreate.cs
+++ HotelBooking/Service/BookingService/BookingCreate.cs
@@ -1,6 +1,5 @@
 ﻿using HotelBooking.Data;
 using HotelBooking.Models;
-using Microsoft.EntityFrameworkCore;
 using Spectre.Console;
 
 namespace HotelBooking.Service.BookingService
@@ -8,42 +7,66 @@ namespace HotelBooking.Service.BookingService
     public class BookingCreate
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly List<Room> _roomsToBook = new List<Room>();
 
         public BookingCreate(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
+
         public void AddBooking(Booking newBooking)
         {
+            newBooking.Rooms = _roomsToBook;
+            foreach (var room in _roomsToBook)
+            {
+                room.IsAvailable = false;
+            }
             _dbContext.Add(newBooking);
             _dbContext.SaveChanges();
         }
 
         public bool BookingExists(int bookingId)
         {
-            return _dbContext.Bookings
-                .Any(booking => booking.Id == bookingId);
+            return _dbContext.Bookings.Any(booking => booking.Id == bookingId);
         }
 
-        public decimal TotalPriceOfBooking()
+        public List<Room> GetRoomsToBook()
         {
-            return _dbContext.Bookings
-                .Include(booking => booking.Rooms)
-                    .Select(booking => new
-                    {
-                        TotalBookingPrice = booking.Rooms
-                        .Sum(room => room.PricePerNight * 
-                        (booking.CheckOutDate - booking.CheckInDate).Days)
-                    })
-                    .Sum(booking => booking.TotalBookingPrice);
+            return _roomsToBook;
+        }
+
+        public decimal TotalPriceOfBooking(DateTime checkInDate, DateTime checkOutDate)
+        {
+            int totalDays = (checkOutDate - checkInDate).Days;
+            return _roomsToBook.Sum(r => r.PricePerNight * totalDays);
         }
 
         public bool IsRoomBooked(int roomNumber)
         {
-            return _dbContext.Rooms
-                .Where(r => r.RoomNumber == roomNumber)
-                .Select(r => !r.IsAvailable)
-                .FirstOrDefault();
+            var room = _dbContext.Rooms.First(r => r.RoomNumber == roomNumber);
+            if (room == null)
+            {
+                return false;
+            }
+            return !room.IsAvailable;
+        }
+
+        public void AddRoomToBooking(string roomNumber, bool extraBed = false)
+        {
+            var room = _dbContext.Rooms
+                .First(r => r.RoomNumber == int.Parse(roomNumber));
+            if (extraBed && room.ExtraBedsCount.HasValue)
+            {
+                if (room.RoomSize >= 25)
+                {
+                    room.ExtraBedsCount += 2;
+                }
+                else
+                {
+                    room.ExtraBedsCount += 1;
+                }
+            }
+            _roomsToBook.Add(room);
         }
     }
 }
