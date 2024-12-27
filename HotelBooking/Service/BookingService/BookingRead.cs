075bc70d97b914f1f2de8f96a315dc6ba245c0d0
Tova Dalin <tova.dalin@hotmail.com>
2024-12-27 16:12:02 +0100

    Refactor BookingRead class and update using directives

--- HotelBooking/Service/BookingService/BookingRead.cs
+++ HotelBooking/Service/BookingService/BookingRead.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HotelBooking.Data;
+using HotelBooking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace HotelBooking.Service.BookingService
 {
-    internal class BookingRead
+    public class BookingRead
     {
+        private readonly ApplicationDbContext _dbContext;
+        public BookingRead(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Booking> GetAllBookingsInDb()
+        {
+            return _dbContext.Bookings.ToList();
+        }
+
+        public IEnumerable<Booking> GetAllActiveBookings()
+        {
+            return _dbContext.Bookings
+                .Where(b => b.Status == StatusOnBooking.Active)
+                .Include(b => b.Invoice)
+                .Include(b => b.Rooms)
+                .Include(b => b.Customer)
+                .OrderBy(b => b.Id)
+                .ToList();
+        }
+
+        public IQueryable<Booking> GetBookingDetails(int id)
+        {
+            return _dbContext.Bookings
+                .Where(b => b.Id == id);
+        }
+
+        public IEnumerable<Booking> GetBookingsDeleted()
+        {
+            return _dbContext.Bookings
+                .Where(b => b.Status == StatusOnBooking.Deleted);
+        }
     }
 }
