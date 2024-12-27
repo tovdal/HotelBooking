b841f0e5c99dbe5f6592a664b625ec4635dce8da
Tova Dalin <tova.dalin@hotmail.com>
2024-12-27 17:06:22 +0100

    Add Validator - ValidatorBookingID. Refactor DisplayCustomerInformation and DisplayRoomInformation. Modify ConsoleMessagePrinter.

---
+++ HotelBooking/Utilities/Validators/ValidatorBookingId.cs
@@ -0,0 +1,19 @@
+﻿namespace HotelBooking.Utilities.Validators
+{
+    public class ValidatorBookingId
+    {
+        public static bool TryGetBookignId(out int bookingId)
+        {
+            Console.WriteLine("Enter the ID of the Booking: ");
+            var stringCustomerID = Console.ReadLine();
+
+            if (!int.TryParse(stringCustomerID, out bookingId))
+            {
+                Console.WriteLine("Please enter a valid number ID.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
