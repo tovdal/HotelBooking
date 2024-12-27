d46c3dfcde3fb76c67907f9582a148a9cf9b21cf
Tova Dalin <tova.dalin@hotmail.com>
2024-12-27 20:05:04 +0100

    Add BookingUpdate and ReturnBookingWithId

--- HotelBooking/Service/BookingService/BookingUpdate.cs
+++ HotelBooking/Service/BookingService/BookingUpdate.cs
@@ -1,14 +1,22 @@
 ﻿using HotelBooking.Data;
+using HotelBooking.Models;
 
 namespace HotelBooking.Service.BookingService
 {
     public class BookingUpdate
     {
         private readonly ApplicationDbContext _dbContext;
+
         public BookingUpdate(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
+
+        public Booking ReturnBookingWithId(int id)
+        {
+            return _dbContext.Bookings.FirstOrDefault(b => b.Id == id);
+        }
+
         public void SaveChanges()
         {
             _dbContext.SaveChanges();
