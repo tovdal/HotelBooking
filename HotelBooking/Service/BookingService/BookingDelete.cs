2e133f981e75c31becdcca593842652a6369a0f1
Tova Dalin <tova.dalin@hotmail.com>
2024-12-28 16:50:46 +0100

    Add db context and HasBooking method to BookingDelete

--- HotelBooking/Service/BookingService/BookingDelete.cs
+++ HotelBooking/Service/BookingService/BookingDelete.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelBooking.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,16 @@ namespace HotelBooking.Service.BookingService
 {
     internal class BookingDelete
     {
-        // SoftDelete
+        private readonly ApplicationDbContext _dbContext;
+        public BookingDelete(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public bool HasBooking(int customerId)
+        {
+            return _dbContext.Customers
+                .Any(c => c.Id == customerId && c.Bookings.Any());
+        }
 
     }
 }
