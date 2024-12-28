73a01b06c77d09c56109c353d5a1be53a1ebb593
Tova Dalin <tova.dalin@hotmail.com>
2024-12-28 19:13:33 +0100

    Update invoice queries and add new method for unpaid invoices.

--- HotelBooking/Service/InvoiceService/InvoiceRead.cs
+++ HotelBooking/Service/InvoiceService/InvoiceRead.cs
@@ -1,5 +1,6 @@
 ﻿using HotelBooking.Data;
 using HotelBooking.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelBooking.Service.InvoiceService
 {
@@ -19,14 +20,29 @@ namespace HotelBooking.Service.InvoiceService
         public IEnumerable<Invoice> GetAllActiveInvoices()
         {
             return _dbContext.Invoices
-                .Where(i => i.IsPaid == false)
+                .Include(i => i.Booking)
+                .Include(i => i.Booking.Rooms)
+                .Include(i => i.Booking.Customer)
                 .OrderBy(i => i.Id)
                 .ToList();
         }
-        public IEnumerable<Invoice> GetAllPayedInvoices()
+        public IEnumerable<Invoice> GetAllPaidInvoices()
         {
             return _dbContext.Invoices
                 .Where(i => i.IsPaid == true)
+                .Include(i => i.Booking)
+                .Include(i => i.Booking.Rooms)
+                .Include(i => i.Booking.Customer)
+                .OrderBy(i => i.Id)
+                .ToList();
+        }
+        public IEnumerable<Invoice> GetAllNotPaidInvoices()
+        {
+            return _dbContext.Invoices
+                .Where(i => i.IsPaid == false)
+                .Include(i => i.Booking)
+                .Include(i => i.Booking.Rooms)
+                .Include(i => i.Booking.Customer)
                 .OrderBy(i => i.Id)
                 .ToList();
         }
