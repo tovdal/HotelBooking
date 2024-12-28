38e834cdbc14464ff7e2a4034e41257f977b5010
Tova Dalin <tova.dalin@hotmail.com>
2024-12-28 15:25:04 +0100

    Modify CustomerSeeding to new model CustomerUpdate

--- HotelBooking/Data/Seeders/CustomerSeeder.cs
+++ HotelBooking/Data/Seeders/CustomerSeeder.cs
@@ -6,85 +6,96 @@ namespace HotelBooking.Data.Seeders
     {
         public void CustomerSeeding(ApplicationDbContext dbContext)
         {
-
-            var customers = new List<Customer>
-            {
-                new Customer
+            var addresses = new List<Address>
                 {
-                    FirstName = "Chris",
-                    LastName = "Bäck",
-                    Email = "Chris.Bäck@live.com",
-                    PhoneNumber = "0734567890",
-                    Address = new Address
+                    new Address
                     {
                         Street = "123 Storgatan",
                         City = "Sundsvall",
                         PostalCode = "85230",
                         Country = "Sweden"
                     },
-                    IsCustomerDeleted = false
-                },
-                new Customer
-                {
-                    FirstName = "Jennifer",
-                    LastName = "Larsson",
-                    Email = "jennifer.Larsson@yahoo.com",
-                    PhoneNumber = "0726543210",
-                    Address = new Address
+                    new Address
                     {
                         Street = "456 Vintervägen",
                         City = "Happaranda",
                         PostalCode = "95331",
                         Country = "Sweden"
                     },
-                    IsCustomerDeleted = false
-                },
-                new Customer
-                {
-                    FirstName = "Wilma",
-                    LastName = "Johnson",
-                    Email = "Wilma.johnson@outlook.com",
-                    PhoneNumber = "0701234567",
-                    Address = new Address
+                    new Address
                     {
                         Street = "789 FrenchSteet",
                         City = "Québec",
                         PostalCode = "G1A 1A1",
                         Country = "Canada"
                     },
-                    IsCustomerDeleted = false
-                },
-                new Customer
-                {
-                    FirstName = "Bob",
-                    LastName = "Ross",
-                    Email = "bob.ross@gmail.com",
-                    PhoneNumber = "0107654321",
-                    Address = new Address
+                    new Address
                     {
                         Street = "321 Pine St",
                         City = "Woodsland",
                         PostalCode = "12345",
                         Country = "USA"
                     },
-                    IsCustomerDeleted = false
-                },
-                new Customer
-                {
-                    FirstName = "Raphael",
-                    LastName = "Andersson",
-                    Email = "raphael.Andersson@hotmail.com",
-                    PhoneNumber = "0702345678",
-                    Address = new Address
+                    new Address
                     {
                         Street = "1 Sand",
                         City = "Oasis",
                         PostalCode = "54321",
-                        Country = "Unknown"
+                        Country = "Desertland"
+                    }
+                };
+
+            dbContext.Address.AddRange(addresses);
+            dbContext.SaveChanges();
+
+            var customers = new List<Customer>
+                {
+                    new Customer
+                    {
+                        FirstName = "Chris",
+                        LastName = "Bäck",
+                        Email = "Chris.Bäck@live.com",
+                        PhoneNumber = "0734567890",
+                        AddressId = addresses[0].Id,
+                        IsCustomerDeleted = false
                     },
-                    IsCustomerDeleted = false
-                }
-            };
+                    new Customer
+                    {
+                        FirstName = "Jennifer",
+                        LastName = "Larsson",
+                        Email = "jennifer.Larsson@yahoo.com",
+                        PhoneNumber = "0726543210",
+                        AddressId = addresses[1].Id,
+                        IsCustomerDeleted = false
+                    },
+                    new Customer
+                    {
+                        FirstName = "Wilma",
+                        LastName = "Johnson",
+                        Email = "Wilma.johnson@outlook.com",
+                        PhoneNumber = "0701234567",
+                        AddressId = addresses[2].Id,
+                        IsCustomerDeleted = false
+                    },
+                    new Customer
+                    {
+                        FirstName = "Bob",
+                        LastName = "Ross",
+                        Email = "bob.ross@gmail.com",
+                        PhoneNumber = "0107654321",
+                        AddressId = addresses[3].Id,
+                        IsCustomerDeleted = false
+                    },
+                    new Customer
+                    {
+                        FirstName = "Raphael",
+                        LastName = "Andersson",
+                        Email = "raphael.Andersson@hotmail.com",
+                        PhoneNumber = "0702345678",
+                        AddressId = addresses[4].Id,
+                        IsCustomerDeleted = false
+                    }
+                };
 
             dbContext.Customers.AddRange(customers);
             dbContext.SaveChanges();
