597e43fe7409e2570d945b8d35cad6dda6e11ca2
Tova Dalin <tova.dalin@hotmail.com>
2024-12-28 19:49:43 +0100

    Modify InvoiceReadController, ShowAllInvoices();, ShowAllNotPaid(); ,ShowAllPaid(); , ShowAInvoiceDetails();

--- HotelBooking/Controllers/ControllerInvoice/InvoiceReadController.cs
+++ HotelBooking/Controllers/ControllerInvoice/InvoiceReadController.cs
@@ -1,5 +1,10 @@
 ﻿using HotelBooking.Controllers.ControllerInvoice.Interfaces;
+using HotelBooking.Models;
+using HotelBooking.Service.BookingService;
 using HotelBooking.Service.InvoiceService;
+using HotelBooking.Utilities.Display.DisplayInformation;
+using HotelBooking.Utilities.Display.Message;
+using HotelBooking.Utilities.Validators;
 
 namespace HotelBooking.Controllers.ControllerInvoice
 {
@@ -12,15 +17,46 @@ namespace HotelBooking.Controllers.ControllerInvoice
         }
         public void ShowAllInvoices()
         {
-            var invoices = _invoiceRead.GetAllInvoices();
+            var invoices = _invoiceRead.GetAllActiveInvoices();
+            DisplayInvoiceInformation.PrintInvoiceAll
+                (invoices, "There are no invoices");
+            ConsoleMessagePrinter.DisplayMessage();
         }
         public void ShowAllNotPaid()
         {
-            throw new System.NotImplementedException();
+            var invoices = _invoiceRead.GetAllNotPaidInvoices();
+            DisplayInvoiceInformation.PrintInvoiceAll(invoices, 
+                "There are no invoices");
+            ConsoleMessagePrinter.DisplayMessage();
+
         }
         public void ShowAllPaid()
         {
-            throw new System.NotImplementedException();
+            var invoices = _invoiceRead.GetAllPaidInvoices();
+            DisplayInvoiceInformation.PrintInvoiceAll
+                (invoices, "There are no paid invoices");
+            ConsoleMessagePrinter.DisplayMessage();
+        }
+
+        public void ShowAInvoiceDetails()
+        {
+            bool isSearching = true;
+            while (isSearching)
+            {
+                Console.Clear();
+                var invoices = _invoiceRead.GetAllActiveInvoices();
+                DisplayInvoiceInformation.PrintInvoiceIdAndCustomerID(invoices);
+
+                if (!ValidatorBookingId.TryGetBookingId(out int invoiceId))
+                {
+                    continue;
+                }
+                var invoice = _invoiceRead.GetInvoiceDetails(invoiceId);
+                DisplayInvoiceInformation.PrintInvoiceAll(invoices,
+                    $"No invoices found with Id: {invoiceId}.");
+                ConsoleMessagePrinter.DisplayMessage();
+                isSearching = false;
+            }
         }
     }
 }
