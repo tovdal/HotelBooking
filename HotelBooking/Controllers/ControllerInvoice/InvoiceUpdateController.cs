373beae8483bd790b3205e7eb537d7fe4ddaaa50
Tova Dalin <tova.dalin@hotmail.com>
2024-12-30 21:19:10 +0100

    Refactor InvoiceUpdateController for readability. Added ValidatorInvoice to move out validation out from class.

--- HotelBooking/Controllers/ControllerInvoice/InvoiceUpdateController.cs
+++ HotelBooking/Controllers/ControllerInvoice/InvoiceUpdateController.cs
@@ -3,70 +3,67 @@ using HotelBooking.Service.InvoiceService;
 using HotelBooking.Utilities.Display.DisplayInformation;
 using HotelBooking.Utilities.Validators;
 using Spectre.Console;
+namespace HotelBooking.Controllers.ControllerInvoice;
 
-namespace HotelBooking.Controllers.ControllerInvoice
+public class InvoiceUpdateController : IInvoiceUpdateController
 {
-    public class InvoiceUpdateController : IInvoiceUpdateController
+    private readonly InvoiceUpdate _invoiceUpdate;
+    private readonly InvoiceRead _invoiceRead;
+    public InvoiceUpdateController(InvoiceUpdate invoiceUpdate,
+        InvoiceRead invoiceRead)
     {
-        private readonly InvoiceUpdate _invoiceUpdate;
-        private readonly InvoiceRead _invoiceRead;
-        public InvoiceUpdateController(InvoiceUpdate invoiceUpdate,
-            InvoiceRead invoiceRead)
+        _invoiceUpdate = invoiceUpdate;
+        _invoiceRead = invoiceRead;
+    }
+    public void PayAInvoice()
+    {
+        bool isRunning = true;
+        while (isRunning)
         {
-            _invoiceUpdate = invoiceUpdate;
-            _invoiceRead = invoiceRead;
-        }
-        public void PayAInvoice()
-        {
-            bool isRunning = true;
-            while (isRunning)
+            Console.Clear();
+            var invoices = _invoiceRead.GetAllActiveInvoices();
+            DisplayInvoiceInformation.PrintInvoiceIdAndCustomerID
+                (invoices, "No invoices registered. (Press enter to return to menu)");
+
+            if (!ValidatorInvoice.TryGetInvoiceId(out int invoiceId))
             {
-                Console.Clear();
-                var invoices = _invoiceRead.GetAllActiveInvoices();
-                DisplayInvoiceInformation.PrintInvoiceIdAndCustomerID
-                    (invoices, "No invoices registered");
-
-                if (!ValidatorBookingId.TryGetBookingId(out int invoiceId))
-                {
-                    continue;
-                }
-
-                var invoiceToPay = _invoiceUpdate.ReturnInvoiceWithId(invoiceId);
-                if (invoiceToPay == null)
-                {
-                    AnsiConsole.MarkupLine
-                        ($"[bold red]No invoice found with Id: {invoiceId}.[/]");
-                    continue;
-                }
-
-                var invoice = _invoiceRead.GetInvoiceDetails(invoiceId);
-
-                DisplayInvoiceInformation.PrintInvoiceAll
-                    (invoice, $"No invoices found with Id: {invoiceId}.");
-
-                bool confirm = AnsiConsole.Confirm
-                    ("\n[bold yellow]Are all details correct?[/]");
-                if (confirm)
-                {
-                    invoiceToPay.Invoice.IsPaid = true;
-                    _invoiceUpdate.SaveChanges();
-                    AnsiConsole.MarkupLine
-                        ($"[green]Invoice with Id: " +
-                        $"{invoiceId} has been marked as paid.[/]");
-                }
-                else
-                {
-                    AnsiConsole.MarkupLine("[bold red]Payment canceled.[/]");
-                }
-
-                bool addAnother = AnsiConsole.Confirm
-                    ("\nDo you want to pay another invoice?");
-                if (!addAnother)
-                {
-                    isRunning = false;
-                }
-                Console.Clear();
+                isRunning = false;
+                continue;
             }
+
+            var invoiceToPay = _invoiceUpdate.ReturnInvoiceWithId(invoiceId);
+            if (!ValidatorInvoice.ValidateInvoiceForUpdate(invoiceToPay, invoiceId))
+            {
+                continue;
+            }
+
+            var invoice = _invoiceRead.GetInvoiceDetails(invoiceId);
+
+            DisplayInvoiceInformation.PrintInvoiceAll
+                (invoice, $"No invoices found with Id: {invoiceId}.");
+
+            bool confirm = AnsiConsole.Confirm
+                ("\n[bold yellow]Are all details correct?[/]");
+            if (confirm)
+            {
+                invoiceToPay.Invoice.IsPaid = true;
+                _invoiceUpdate.SaveChanges();
+                AnsiConsole.MarkupLine
+                    ($"[green]Invoice with Id: " +
+                    $"{invoiceId} has been marked as paid.[/]");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[bold red]Payment canceled.[/]");
+            }
+
+            bool addAnother = AnsiConsole.Confirm
+                ("\nDo you want to pay another invoice?");
+            if (!addAnother)
+            {
+                isRunning = false;
+            }
+            Console.Clear();
         }
     }
 }
