a8a444581716a8cb26ceeae0fe74df1a992a77ed
Tova Dalin <tova.dalin@hotmail.com>
2024-12-30 18:01:54 +0100

    Add ValidatorCustomer.cs and remove ValidatorCustomerId.cs. Add ValidateCustomerForUpdate, ValidateDeletedCustomerForRestore, ValidateDeletedCustomers

---
+++ HotelBooking/Utilities/Validators/ValidatorCustomer.cs
@@ -0,0 +1,68 @@
+﻿using HotelBooking.Models;
+using HotelBooking.Service.CustomerService;
+using Spectre.Console;
+
+namespace HotelBooking.Utilities.Validators
+{
+    public class ValidatorCustomer
+    {
+        public static bool TryGetCustomerId(out int customerId)
+        {
+            Console.WriteLine("Enter the ID of the Customer: ");
+            var stringCustomerID = Console.ReadLine();
+
+            if (!int.TryParse(stringCustomerID, out customerId))
+            {
+                AnsiConsole.MarkupLine
+                    ("[bold red]Please enter a valid number ID[/]");
+                Console.ReadKey();
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool ValidateCustomerForUpdate(Customer customerToUpdate,
+               int customerId, CustomerDelete customerDelete)
+        {
+            if (customerToUpdate == null)
+            {
+                AnsiConsole.MarkupLine
+                    ($"[bold red]No customer found with ID number: {customerId}.[/]");
+                Console.ReadKey();
+                return false;
+            }
+            if (customerDelete.HasCustomerBooking(customerId))
+            {
+                AnsiConsole.MarkupLine
+                    ("[bold red]The Customer has a booking, can't be updated[/]");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+        public static bool ValidateDeletedCustomerForRestore(Customer customerToUpdate,
+            int customerId)
+        {
+            if (customerToUpdate == null || !customerToUpdate.IsCustomerDeleted)
+            {
+                AnsiConsole.MarkupLine
+                    ($"[bold red]No deleted Customer found with ID number: {customerId}.[/]");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+        public static bool ValidateDeletedCustomers(List<Customer> deletedCustomers)
+        {
+            if (!deletedCustomers.Any())
+            {
+                AnsiConsole.MarkupLine("[bold red]There are no deleted customers.[/]");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+    }
+}
