dc87e2ed2d25167cc0acd79243c3e4ff15ccaeb6
Tova Dalin <tova.dalin@hotmail.com>
2024-12-30 22:54:22 +0100

    MS-SQL syntaxer G

---
+++ HotelBooking/MS-SQL-syntaxer-G.sql
@@ -0,0 +1,29 @@
+--Skapa MS-SQL syntaxer som kan utföra ’Select’, ’Where’, ’Order By’ uppgifter mot din databas
+
+---- SELECT ----
+USE HotelbookingDBTova;
+
+SELECT
+	*
+FROM
+Customers
+
+---- WHERE ----
+USE HotelbookingDBTova;
+
+SELECT 
+    * 
+FROM 
+    Customers AS c
+WHERE 
+    c.FirstName LIKE 'C%';
+
+---- ORDER BY ----
+USE HotelbookingDBTova;
+
+SELECT 
+	*
+FROM
+	Rooms As r
+ORDER BY
+	r.PricePerNight DESC
